83dacefe65d70081c70081bcf5944a46e1f27f14
simon ogorek <so293@njit.edu>
2026-01-14 15:10:12 -0500

    Swapping out the router for HashRouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Home from './Home';
 
 import { BrowserRouter, Routes, Route } from "react-router";
 
+import { HashRouter } from "react-router-dom";
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
